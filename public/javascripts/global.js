31de5c9743f15c6258241e51e10021110351c948
Erica Wright <ericaw21@gmail.com>
2017-06-09 11:25:33 -0700

    add delete user route and function

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -13,6 +13,9 @@ $(document).ready(function() {
     // Add User button click
     $('#btnAddUser').on('click', addUser);
 
+    // Delete User link click
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+
 });
 
 // Functions =================================================
@@ -119,3 +122,36 @@ function addUser(event) {
         return false;
     }
 };
+
+function deleteUser(event) {
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    //Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            } else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+
+        });
+
+    } else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+    }
+}
